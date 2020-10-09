37bc2f1a6206322c576b9ceb41329f0626ac80c7
kpym <kpym.tzanev@gmail.com>
2020-10-09 13:20:28 +0200

    focus auto + prénom avec majuscule + correction d'nu bug

--- signature/main.js
+++ signature/main.js
@@ -15,20 +15,24 @@ var app = new Vue({
   },
   computed: {
     signature: function () {
-      var activateon = this.prenom+this.nom+this.function+this.fix+this.mobile;
+      var activateon = this.prenom+this.nom+this.fonction+this.fix+this.mobile;
       return this.$el.querySelector("#result").innerHTML;
     }
   },
   methods: {
-    br: function (v) {
-      return v.split("\n").join("<br>\n")
+    br: function (txt) {
+      return txt.split("\n").join("<br>\n");
+    },
+    title: function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1);
     },
     toggleCode: function() {
       this.code = !this.code;
     }
   },
-  created: function () {
-    new ClipboardJS('.btn')
+  ready: function () {
+    new ClipboardJS('.btn');
+    this.$el.querySelector("input").focus();
   }
 })
 
