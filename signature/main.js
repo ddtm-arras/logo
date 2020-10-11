ce8105974507000810e67de7e0f9f4b7c4c9ef31
kpym <kpym.tzanev@gmail.com>
2020-10-11 17:40:08 +0200

    correction bug dans dans le générateur de signature

--- signature/main.js
+++ signature/main.js
@@ -11,6 +11,7 @@ var app = new Vue({
     fonction:    '',
     fix:         '',
     mobile:      '',
+    imgType:     'extSVG',
     logoDDTMsvg: logoDDTMsvg,
     logoDDTMpng: logoDDTMpng,
     signature:   null,
@@ -26,19 +27,25 @@ var app = new Vue({
     },
     toggleCode: function() {
       this.code = !this.code;
+    },
+    setSignature: function(){
+      this.signature = this.$el.querySelector("#result").innerHTML;
+      this.copied = false;
     }
   },
   ready: function () {
+    // Initialisation de clipboard.js
     new ClipboardJS('.btn');
-    this.$el.querySelector("input").focus();
+    // surveiller toutes les variantes pour mettre à jour la signature
     this.$watch(
       ()=>this.prenom+this.nom+this.fonction+this.fix+this.mobile+this.imgType,
-      function(nv,ov){
-        this.signature = this.$el.querySelector("#result").innerHTML;
-        this.copied = false;
-      },
+      this.setSignature,
       { immediate: true }
     );
+    // initialisation de la signature pour la première fois (probablement inutile)
+    this.setSignature();
+    // mettre le focus dans le premier champs à remplir (le Prénom)
+    this.$el.querySelector("input").focus();
   }
 })
 
